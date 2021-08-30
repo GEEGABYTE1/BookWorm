ebc818ebdd3a8da8b0af676f0ba719dbfe931408
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-08-30 13:48:02 -0400

    Homepage design

--- script.py
+++ script.py
@@ -1,1 +1,25 @@
 from Tree import*
+
+class Running:
+
+    def running(self):
+        pass 
+
+    
+    def homepage(self):
+        print("-"*49)
+        print("*"  + "\t" +  "\t" + "\t" +  "\t" +  "\t" +  "\t" + "*" + "\n")
+        print("*"  + "\t" +  "\t" + "\t" +  "\t" +  "\t" +  "\t" + "*")
+        print("\t" + "     Welcome to BookWorm!")
+        print("*"  + "\t" +  "\t" + "\t" +  "\t" +  "\t" +  "\t" + "*" + "\n")
+        print("*"  + "\t" +  "\t" + "\t" +  "\t" +  "\t" +  "\t" + "*" + "\n")
+        print("-"*49)
+       
+
+
+
+
+
+test = Running()
+
+print(test.homepage())

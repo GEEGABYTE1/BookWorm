5c2a53c07eab24edb2fb9fb434b2cded1b272758
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-08-30 13:52:00 -0400

    Initial Command and Landing message

--- script.py
+++ script.py
@@ -1,10 +1,23 @@
 from Tree import*
+import time
 
 class Running:
 
     def running(self):
-        pass 
-
+        self.homepage()
+        print("\n")
+        print("Currently, there are only 3 genres of books you can search from:")
+        time.sleep(0.5)
+        print("Math, Computer, and Physics!")
+        time.sleep(0.8)
+        print("There will be new genres coming soon!")
+        time.sleep(0.3)
+        
+        
+        while True:
+            print("\n")
+            print("Please type in a command:")
+            prompt = str(input(":"))
     
     def homepage(self):
         print("-"*49)
@@ -22,4 +35,4 @@ class Running:
 
 test = Running()
 
-print(test.homepage())
+print(test.running())

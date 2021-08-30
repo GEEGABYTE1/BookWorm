77d7a704a5290a01e1683498f82ac62095c328e3
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-08-30 14:17:33 -0400

    Spelling

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 math = {'Introduction to Graph Theory': ['Richard J. Trudeau', 16.85], 'Elements of Algebra': ['Leonhard Euler', 26.18], 'Zero: The Biography of a Dangerous Idea':['Charles Seife', 19], 'A Course of Pure Mathematics': ['G.H Hardy', 20]}
 com_sci = {'Code: The Hidden Language of Computer Hardware and Software': ['Charles Petzold', 31.37], 'Computer Science Distilled': ['Wladston Ferreira Filho', 30], 'Algorithms to Live By': ['Brain Christian', 28.46], 'The Ethical Algorithm':['Micheal Kearns', 28.56]}
-physics = {'Ignition! An Informal History of Liquid Rocket Propellants':['John Drury Clark', 30.64], 'Feynman\'\'s Tips on Physics': ['Richard Feynman', 20], 'Six Not-So-Easy Pieces':['Richard Feynman', 21]}
+physics = {'Ignition! An Informal History of Liquid Rocket Propellants':['John Drury Clark', 30.64], 'Feynman\'s Tips on Physics': ['Richard Feynman', 20], 'Six Not-So-Easy Pieces':['Richard Feynman', 21]}
 
 

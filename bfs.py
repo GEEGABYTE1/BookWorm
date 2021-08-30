6b2e5a262951cb8114bd0038a14baffed948a44b
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-08-30 13:54:52 -0400

    BFS implementation

---
+++ bfs.py
@@ -0,0 +1,21 @@
+from Tree import TreeNode
+from collections import deque 
+
+def bfs(root_node, goal):
+    path_queue = deque()
+    initial_path = [root_node]
+    path_queue.appendleft(initial_path)
+
+    while path_queue:
+        current_path = path_queue.pop()
+        current_node = current_path[-1]
+        print("Searchin Book with title: {}".format(current_node.value[0]))
+        if current_node.value == goal:
+            return current_path 
+        else:
+            for child in current_node.children:
+                new_path = current_path[:]
+                new_path.append(child)
+                path_queue.appendleft(new_path)
+    
+    return None

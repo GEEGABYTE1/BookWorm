e48032a7022589b39f4f96c59eb7da638f596525
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-08-31 14:50:53 -0400

    Bfs_Author Test1

---
+++ bfs_author.py
@@ -0,0 +1,19 @@
+from collections import deque 
+from Tree import TreeNode 
+
+
+def bfs_author(root_node, goal):
+    path_queue = deque()
+    initial_path = [root_node]
+    path_queue.appendleft(initial_path)
+    while path_queue:
+        current_path = path_queue.pop()
+        current_node = current_path[-1]
+        if current_node.value[0] == goal:
+            return current_path 
+        else:
+            for child in current_node.children:
+                new_path = current_path[:]
+                new_path.append(child)
+                path_queue.appendleft(new_path)
+

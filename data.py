0921334d5ccd7d9a93b047f0c58ea65aa2de367a
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-08-31 14:50:24 -0400

    Title Changes to Author

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
-math = {'Introduction to Graph Theory'.title(): ['Richard J. Trudeau', 16.85], 'Elements of Algebra'.title(): ['Leonhard Euler', 26.18], 'Zero: The Biography of a Dangerous Idea'.title():['Charles Seife', 19], 'A Course of Pure Mathematics'.title(): ['G.H Hardy', 20]}
-com_sci = {'Code: The Hidden Language of Computer Hardware and Software'.title(): ['Charles Petzold', 31.37], 'Computer Science Distilled'.title(): ['Wladston Ferreira Filho', 30], 'Algorithms to Live By'.title(): ['Brain Christian', 28.46], 'The Ethical Algorithm'.title():['Micheal Kearns', 28.56]}
-physics = {'Ignition! An Informal History of Liquid Rocket Propellants'.title():['John Drury Clark', 30.64], 'Feynman\'s Tips on Physics'.title(): ['Richard Feynman', 20], 'Six Not-So-Easy Pieces'.title():['Richard Feynman', 21]}
+math = {'Introduction to Graph Theory'.title(): ['Richard J. Trudeau'.title(), 16.85], 'Elements of Algebra'.title(): ['Leonhard Euler'.title(), 26.18], 'Zero: The Biography of a Dangerous Idea'.title():['Charles Seife'.title(), 19], 'A Course of Pure Mathematics'.title(): ['G.H Hardy'.title(), 20]}
+com_sci = {'Code: The Hidden Language of Computer Hardware and Software'.title(): ['Charles Petzold'.title(), 31.37], 'Computer Science Distilled'.title(): ['Wladston Ferreira Filho'.title(), 30], 'Algorithms to Live By'.title(): ['Brain Christian'.title(), 28.46], 'The Ethical Algorithm'.title():['Micheal Kearns'.title(), 28.56]}
+physics = {'Ignition! An Informal History of Liquid Rocket Propellants'.title():['John Drury Clark'.title(), 30.64], 'Feynman\'s Tips on Physics'.title(): ['Richard Feynman'.title(), 20], 'Six Not-So-Easy Pieces'.title():['Richard Feynman'.title(), 21]}
 
 
 dictionaries = [math, com_sci, physics]
